120c2d17a616b2206a9b49e95ad8b50ba093dd21
ksereda <ksereda2017@gmail.com>
2018-10-21 14:58:24 +0300

    added "Pair" task

---
+++ src/lesson_6/generics_6_1/Pair.java
@@ -0,0 +1,59 @@
+package lesson_6.generics_6_1;
+
+import java.util.Objects;
+
+public class Pair<X, Y> {
+
+    private X first;
+    private Y second;
+
+    private Pair(X first, Y second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public X getFirst() {
+        return first;
+    }
+
+    public Y getSecond() {
+        return second;
+    }
+
+    public static <X, Y> Pair <X, Y> of(X first, Y second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(first, second);
+    }
+
+    public static void main(String[] args) {
+        Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst(); // 1
+        String s = pair.getSecond(); // "hello"
+
+        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        boolean mustBeTrue = pair.equals(pair2); // true!
+        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+
+        System.out.println(i);
+        System.out.println(s);
+
+        System.out.println(mustBeTrue);
+        System.out.println(mustAlsoBeTrue);
+    }
+
+}
+
