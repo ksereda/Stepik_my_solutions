190ea763a9be4b3dc6e268d2871c754351a70677
ksereda <ksereda2017@gmail.com>
2018-11-03 18:44:07 +0300

    added "Symmetric Difference" task

---
+++ src/collections_6_2/SymmetricDifference.java
@@ -0,0 +1,36 @@
+package collections_6_2;
+
+import java.util.*;
+
+public class SymmetricDifference {
+
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+
+        Set<T> setResult = new HashSet<>(set1);
+        setResult.removeAll(set2);
+        Set<T> tempSet = new HashSet<>(set2);
+        tempSet.removeAll(set1);
+        setResult.addAll(tempSet);
+
+        return setResult;
+    }
+
+    public static void main(String[] args) {
+        Set<String> set1 = new HashSet<>();
+        set1.add("1");
+        set1.add("2");
+        set1.add("3");
+        System.out.println("Первое множество: " + set1);
+
+        Set<String> set2 = new HashSet<>();
+        set2.add("0");
+        set2.add("1");
+        set2.add("2");
+        System.out.println("Второе множество: " + set2);
+
+        System.out.println("Разность множеств: " + symmetricDifference(set1, set2));
+    }
+}
+
+
+
