bee188be5f57526ef1e81d14961717200e8129de
ksereda <ksereda2017@gmail.com>
2018-11-03 18:46:24 +0300

    added "Symmetric Difference" task

--- src/collections_6_2/SymmetricDifference.java
+++ src/collections_6_2/SymmetricDifference.java
@@ -2,6 +2,14 @@ package collections_6_2;
 
 import java.util.*;
 
+/* Реализуйте метод, вычисляющий симметрическую разность двух множеств.
+
+Метод должен возвращать результат в виде нового множества. Изменять переданные в него множества не допускается.
+
+Пример:
+Симметрическая разность множеств {1, 2, 3} и {0, 1, 2} равна {0, 3}.
+ */
+
 public class SymmetricDifference {
 
     public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
