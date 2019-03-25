814d829bed5bfc4576474dd66e99ae42504bd1b4
ksereda <ksereda2017@gmail.com>
2019-03-25 16:46:41 +0300

    added task 1 module 2.4

---
+++ src/module_4/task_1/Main.java
@@ -0,0 +1,26 @@
+package module_4.task_1;
+
+/*
+Реализуйте метод, вычисляющий факториал заданного натурального числа.
+
+Факториал 𝑁 вычисляется как 1⋅2⋅...⋅𝑁.
+
+Поскольку это очень быстро растущая функция, то даже для небольших 𝑁 вместимости типов int и long очень скоро не хватит. Поэтому будем использовать BigInteger.
+*/
+
+import java.math.BigInteger;
+
+public class Main {
+
+    public static BigInteger factorial(int value) {
+
+        BigInteger resultNumber = BigInteger.ONE;
+
+        for (int i = 1; i <= value; i++) {
+            resultNumber = resultNumber.multiply(BigInteger.valueOf(i));
+        }
+        return resultNumber;
+
+    }
+
+}
