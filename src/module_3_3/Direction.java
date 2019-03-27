1c4d2e6bd97f5a436f1d79cd32a0f16d5c187fdb
ksereda <ksereda2017@gmail.com>
2019-03-27 14:41:33 +0300

    added task 1 module 3.3

---
+++ src/module_3_3/Direction.java
@@ -0,0 +1,10 @@
+package module_3_3;
+
+public enum Direction {
+
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+
+}
---
+++ src/module_3_3/Robot.java
@@ -0,0 +1,33 @@
+package module_3_3;
+
+public class Robot {
+
+    public Direction getDirection() {
+        // текущее направление взгляда
+        return null;  // для примера
+    }
+
+    public int getX() {
+        // текущая координата X
+        return 3;  // для примера (по условию задания)
+    }
+
+    public int getY() {
+        // текущая координата Y
+        return 0;  // для примера (по условию задания)
+    }
+
+    public void turnLeft() {
+        // повернуться на 90 градусов против часовой стрелки
+    }
+
+    public void turnRight() {
+        // повернуться на 90 градусов по часовой стрелке
+    }
+
+    public void stepForward() {
+        // шаг в направлении взгляда
+        // за один шаг робот изменяет одну свою координату на единицу
+    }
+
+}
