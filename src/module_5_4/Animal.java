cb23132fe1de2d75637cc3258a7b3724d8a5c684
ksereda <k.sereda@softteco.com>
2019-04-04 21:08:20 +0300

    added task 1 in module 5.4

---
+++ src/module_5_4/Animal.java
@@ -0,0 +1,22 @@
+package module_5_4;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+class Animal implements Serializable {
+
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Animal) {
+            return Objects.equals(name, ((Animal) obj).name);
+        }
+        return false;
+    }
+
+}
